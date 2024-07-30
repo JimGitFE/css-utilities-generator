c12823ce842a2cf8bf3225edd29e55bc7e62b20c
JimGitFE <rymsjfjrys@gmail.com>
2024-07-30 00:30:19 -0300

    add: quick testing of filterClasses method

---
+++ tests/test.ts
@@ -0,0 +1,9 @@
+import { filterClasses } from "../helpers";
+
+console.log(filterClasses(["c--color-functional-alert-100"])) // {color: var(--color-functional-alert-100)}
+console.log(filterClasses(["color--color-functional-alert-100"])) // {color: var(--color-functional-alert-100)}
+
+console.log(filterClasses(["d-flex"])) // {display: flex}
+console.log(filterClasses(["d-f"])) // {display: flex}
+console.log(filterClasses(["display-f"])) // {display: flex}
+console.log(filterClasses(["display-flex"])) // {display: flex}
