f07676f8d97f266b9696b2f4240a4fe93113b3f3
JimGitFE <rymsjfjrys@gmail.com>
2024-08-06 01:57:12 -0300

    add: directory defaults - updated readme default paths

--- generator.ts
+++ generator.ts
@@ -1,12 +1,12 @@
 #!/usr/bin/env ts-node
-import { getFilePaths, generateAST, extractClasses, filterClasses, writeCSS, ProcessRetriever, readConfigFile } from './helpers';
+import { getFilePaths, generateAST, extractClasses, filterClasses, writeCSS, readConfigFile } from './helpers';
 
 // const flags = new ProcessRetriever(process);
 
 /** Directories. Write: generated css utils. Read: classNames to interpret */
 const directory = {
-  writeTo: readConfigFile()?.writeTo || "styles/utilities.css",
-  readFrom: readConfigFile()?.readFrom || "/"
+  writeTo: readConfigFile()?.writeTo || "./src/styles/utilities.css",
+  readFrom: readConfigFile()?.readFrom || "./src/"
 }
 let rawClasses: string[] = []
 
