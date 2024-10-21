e0f046f3056818eb85fa5ce44142da28196bc115
JimGitFE <rymsjfjrys@gmail.com>
2024-10-21 20:01:09 -0300

    add: in package nodemon

--- watch.ts
+++ watch.ts
@@ -1,17 +1,20 @@
 import { spawn } from 'child_process';
 import * as path from 'path';
 import { readConfigFile } from './helpers';
+import { getDirectories } from './utils';
 
-const { readFrom = "./", extensions = "tsx,ts,js,jsx" } = readConfigFile()
+const { exclude = [], readFrom = "./", extensions = "tsx,ts,js,jsx" } = readConfigFile()
 
 // Spawn the nodemon process
 const nodemonProcess = spawn('npx', [
     'nodemon',
-    '--watch', path.resolve(readFrom, "../.."),
+    '--watch', readFrom,
     '--ext', extensions,
-    '--exec', 'css-utilities-generator generate'
+    '--exec', 'css-utilities-generator generate',
+    ...exclude.flatMap(pattern => ['--ignore', pattern])
   ], {
-    stdio: 'inherit'
+    stdio: 'inherit',
+    shell: true,
   });
 
 nodemonProcess.on('error', (err) => {
