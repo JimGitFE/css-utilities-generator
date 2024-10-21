de5b5c698792f9edf79f0f893b8131ad2a55d407
JimGitFE <rymsjfjrys@gmail.com>
2024-10-21 18:42:28 -0300

    typo

--- check.ts
+++ check.ts
@@ -38,9 +38,9 @@ classes.forEach(({ fullClass, classKey, classValue }) => {
 const currentCSS = fs.readFileSync(writeTo, 'utf-8').trim();
 
 if (currentCSS.replace(/\s+/g, '') === generatedCSS.replace(/\s+/g, '')) {
-    console.log('Generated css utilities are up to date')
+    console.log('Generated css utilities are up to date.')
     process.exit(0)
 } else {
-    console.error('Generated css utilities are outdated')
+    console.error('Generated css utilities are outdated.')
     process.exit(1)
 }
